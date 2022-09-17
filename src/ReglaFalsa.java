cdc2aafb3942cc43a86cccb1dc22fabb04384ca2
Jk <juanandrs.montoyagaleano@gmail.com>
2022-09-17 15:04:11 -0500

    BI, Bisección, PuntoFijo, ReglaFalsa

---
+++ src/ReglaFalsa.java
@@ -0,0 +1,11 @@
+import java.lang.Math;
+
+public class ReglaFalsa {
+    public static void main (String[] args){
+
+    }
+
+    public static void RF(double a, double b, double tol, double Nmax){
+
+    }
+}
